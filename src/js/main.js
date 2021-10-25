fa10449c8fe7bd0723096e6cf1d0841264ed8f80
anderson2dev <loyolaandersom2013@gmail.com>
2021-10-25 11:07:24 -0300

    Adds v1

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+/*
+    Defina o objeto base para a criação da aplicação vue 
+*/
+const fans = {
+    data() {
+        return {
+            vueLogo: 'https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg',
+            fanCount: 0,
+            vueFans: [],
+            name: "",
+        };
+    },
+    methods: {
+        addFan()  {
+            if(this.name !== "") {
+                const existingFan =  this.vueFans.find(fan => fan.name === this.name)
+                if (existingFan) { return }
+                this.vueFans.push({
+                    id: this.fanCount,
+                    name: this.name
+                });
+            } else {
+                window.alert('Fan name is empty');
+            }
+            fanName="";
+            this.fanCount++;
+        }
+    }
+};
+
+const app = Vue.createApp(fans);
+const vm = app.mount('#fans');
+
+/*
+    Crie o app e monte ele na div especificada
+*/
